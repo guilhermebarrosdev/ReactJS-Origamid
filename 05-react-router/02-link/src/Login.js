2b3cf4d7b4d4738d339cee6195914c8eaba7a5f3
Guilherme Barros <wingui3@gmail.com>
2022-01-20 17:25:16 -0300

    :memo: Add files link

---
+++ 05-react-router/02-link/src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/');
+    console.log('Fazer login');
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={handleClick}>Login</button>
+    </div>
+  );
+};
+
+export default Login;
