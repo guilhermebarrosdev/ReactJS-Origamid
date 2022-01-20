1adc422c782a2f8ed4ee64f6a997520e0b7e8d35
Guilherme Barros <wingui3@gmail.com>
2022-01-20 17:50:00 -0300

    :memo: Add files useparams

---
+++ 05-react-router/03-useparams/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Login from './Login';
+import NaoEncontrada from './NaoEncontrada';
+import Produto from './Produto';
+import Sobre from './Sobre';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="*" element={<NaoEncontrada />} />
+        <Route path="/produto/:id" element={<Produto />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
