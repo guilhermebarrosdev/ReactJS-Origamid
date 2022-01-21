7f3a811722f39b3a210c551396fb2f63b1e0d4ee
Guilherme Barros <wingui3@gmail.com>
2022-01-20 23:51:45 -0300

    :memo: Create folder mais-react and add files proptypes

---
+++ 06-mais-react/01-proptypes/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from './Button';
+
+const App = () => {
+  return (
+    <div>
+      <Button width={300}>Clique aqui</Button>
+    </div>
+  );
+};
+
+export default App;
