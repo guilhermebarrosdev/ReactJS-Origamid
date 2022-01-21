67ce9fb41e9cb247d8af3a1386be35b418ecf6fb
Guilherme Barros <wingui3@gmail.com>
2022-01-21 00:29:57 -0300

    :memo: Add files lazy and suspense

---
+++ 06-mais-react/02-lazy-e-suspense/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const Contato = React.lazy(() => import('./Contato'));
+
+const App = () => {
+  const [ativo, setAtivo] = React.useState(false);
+
+  return (
+    <div>
+      {ativo && (
+        <React.Suspense fallback={<div>Carregando...</div>}>
+          <Contato />
+        </React.Suspense>
+      )}
+      <button onClick={() => setAtivo(true)}>Ativar</button>
+    </div>
+  );
+};
+
+export default App;
